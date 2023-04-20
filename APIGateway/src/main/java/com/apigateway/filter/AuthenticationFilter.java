fe6946d99ff4721477508bc6c7c40af4c898a562
glp-92 <guillermolopezpazos@gmail.com>
2023-04-20 19:13:11 +0200

    minor changes

--- APIGateway/src/main/java/com/apigateway/filter/AuthenticationFilter.java
+++ APIGateway/src/main/java/com/apigateway/filter/AuthenticationFilter.java
@@ -1,28 +1,17 @@
 package com.apigateway.filter;
 
-import java.security.Key;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 
 import com.apigateway.util.JwtUtil;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import reactor.core.publisher.Mono;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
 	
-	private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
 	
     @Autowired
--- APIGateway/src/main/java/com/apigateway/filter/RouteValidator.java
+++ APIGateway/src/main/java/com/apigateway/filter/RouteValidator.java
@@ -8,12 +8,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class RouteValidator {
 
+	
     public static final List<String> openApiEndpoints = List.of(
             "/auth/register",
             "/auth/token",
             "/eureka"
     );
 
+    
+    //Takes a stream of openApiEndpoints
+    //Returns true if the uri of the request is not included in that stream and false if is included
     public Predicate<ServerHttpRequest> isSecured =
             request -> openApiEndpoints
                     .stream()
